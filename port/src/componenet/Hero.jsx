9d3b53421dfb59dcb67e06066e916034c69cb20b
neuervinch <npandian515@gmail.com>
2025-01-09 16:16:51 +0530

    llkl

---
+++ port/src/componenet/Hero.jsx
@@ -0,0 +1,31 @@
+import { Canvas } from '@react-three/fiber'
+import React, { Suspense } from 'react'
+
+const Hero = () => {
+  return (
+   <section className='  min-h-screen w-full flex flex-col relative'>
+            <div className='text-black text-9xl py-64'>
+            <p>
+                Hi, I am Naveen Pandian
+                </p>    
+              <p>
+                I am this ! 
+              </p>
+            
+            </div>
+
+            <div>
+
+                
+            </div> 
+            <div>
+                <a href='/'>
+                    <button/>
+                </a>
+            </div>
+            
+   </section>
+  )
+}
+
+export default Hero
