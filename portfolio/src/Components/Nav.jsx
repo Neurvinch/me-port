83465ee40d1cac1e64c75874f0753933c4bd8c57
neuervinch <npandian515@gmail.com>
2025-01-12 13:42:46 +0530

    poi

---
+++ portfolio/src/Components/Nav.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Nav = () => {
+
+
+  return (
+    <div className='bg-black'>
+        header
+        <div>
+            child1
+
+        </div>
+
+        <div>
+
+
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Nav
